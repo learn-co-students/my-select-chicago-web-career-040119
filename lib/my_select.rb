52ce93164f1c02c27efa9772c988a98991108be1
Diana Utan <dia_utzi@yahoo.com>
2019-02-19 01:41:59 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
 def my_select(collection)
- # your code here!
+  # testing if array empty >> optional
+# if collection.empty?
+#     return nil 
+# end
+
+i = 0 
+new_array = [] 
+while i < collection.length
+    if (yield collection[i]) == true
+    new_array.push(collection[i])
+    end
+  i += 1 
+  end
+new_array
 end
